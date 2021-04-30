cb2f353dd5e0b23761834cb4023d5a146f1d92a9
Joonas Uusnäkki <joonas.uusnakki@gmail.com>
2021-04-30 15:30:44 +0300

    time and disconnect button added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './App.css';
 import struct from "./myStruct.js";
 
@@ -6,10 +6,22 @@ import struct from "./myStruct.js";
 function App() {
   // This is the data that we get from the badge. We use states to edit it.
   const [badgedata, setBadgedata] = useState("");
+  const screenTime = new Date().toLocaleTimeString();
+  const date = new Date();
+  const [time, setTime] = useState(screenTime);
 
   //TODO: should be swapped to constanly changing date time: now it is a constant
-  const date = new Date();
   const currentTime = date.getTime();
+
+  function updateClock() {
+    const screenTime = new Date().toLocaleTimeString();
+    setTime(screenTime)
+  }
+
+  // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    window.setInterval(updateClock, 1000)
+  });
 
   const connectToDeviceAndSubscribeToUpdates = async () => {
     try {
@@ -28,12 +40,17 @@ function App() {
       console.log('Device: ' + JSON.stringify(device));
       // Here we ask the server for badges' primary service for notifying and writing. 
       const primaryService = await server.getPrimaryService('6e400001-b5a3-f393-e0a9-e50e24dcca9e')
+
       //This service is for asking the device name for evey 5 seconds
-      const secondaryService = await server.getPrimaryService('00001800-0000-1000-8000-00805f9b34fb')
+      //const secondaryService = await server.getPrimaryService('00001800-0000-1000-8000-00805f9b34fb')
+
       // And here we ask the service for its characteristics --> notify -characteristics in this case. 
       const notifyC = await primaryService.getCharacteristic('6e400003-b5a3-f393-e0a9-e50e24dcca9e');
+
       // Here is a characterristic for asking the device's name
-      const askName = await secondaryService.getCharacteristic('00002a00-0000-1000-8000-00805f9b34fb');
+      //const askName = await secondaryService.getCharacteristic('00002a00-0000-1000-8000-00805f9b34fb');
+
+      // Here is a characteristics for writing values to the badge and
       const writeC = await primaryService.getCharacteristic('6e400002-b5a3-f393-e0a9-e50e24dcca9e');
       console.log(notifyC)
       console.log("Getting the characteristic");
@@ -44,57 +61,70 @@ function App() {
         console.log("Event listener in full action"));
       await notifyC.startNotifications();
       console.log('Notifications have been started.');
-      const alertValue = await askName.readValue()
+      //const alertValue = await askName.readValue()
       //console.log(alertValue)
-
-      // await window.setInterval(communicateWithBadge, 3000)
-
-      //console.log(askName.readValue().getUint8(0))
-
-      //setBadgedata("töttöröö")
+      await sendStartRecRequest()
+      await window.setInterval(communicateWithBadge, 3000)
 
 
-      /*Here some random data for arraybuffer testing.
+
+      /*  sends request to start recording, with specified timeout
+       (if after timeout minutes badge has not seen server, it will stop recording)
+         sendStartRecRequest: badge.py line 441
       This works! Now:
         - badge doesnt disconnect immediately after writing
         - event handler for notifying is activated
       */
-       let s = struct('<cIIH')
-       const bufferTest = new ArrayBuffer(s.size)
-       console.log("s.size is " + s.size)
-       console.log(bufferTest)
-      // console.log(s.size)
-       let jepa = s.pack("1", currentTime, currentTime, 5)
-       console.log("this is jepa" + jepa)
-       console.log(jepa)
-       
+      function sendStartRecRequest() {
+        let s = struct('<cIHH')
+        const buffer = new ArrayBuffer(s.size)
+        console.log("s.size is " + s.size)
+        console.log(buffer)
+        // console.log(s.size)
+        let recRequest = s.pack("1", currentTime, currentTime, 3)
+        console.log("this is recRequest" + recRequest)
+        console.log(recRequest)
+        writeC.writeValue(recRequest)
+      }
 
 
-     /* var bytes = ["r",currentTime];
-      var unpacked = bytes.reduce(function (s, e) { 
-        return s | e << ((2) * 8); 
-      }, 0);
-      console.log(unpacked); // 1700885369
+      /* send request for data since given date
+        Note - date is given in UTC epoch
+         sendDataRequest: badge.py line 466
+      This works! Now:
+        - badge doesnt disconnect immediately after writing
+        - event handler for notifying is activated
       */
+      function sendDataRequest() {
+        let s = struct('<cIH')
+        const bufferTest = new ArrayBuffer(s.size)
+        console.log("s2.size is " + s.size)
+        console.log(bufferTest)
+        // console.log(s.size)
+        let dataRequest = s.pack("r", currentTime, currentTime)
+        console.log("this is dataRequest" + dataRequest)
+        console.log(dataRequest)
+        writeC.writeValue(dataRequest)
+      }
 
-     /* 
-      const buffer = new ArrayBuffer(3)
-      const dataView = new DataView(buffer);
-      console.log(buffer)
-      dataView.setInt8(0, 256, unpacked)
 
-      const data0 = dataView.getInt8(0)
-      const data1 = dataView.getInt8(1)
-      console.log("Date time in Epoch: " + currentTime)
-      console.log(data0)
-      console.log(data1)
-      console.log(buffer)
-      */
+      /* 
+       const buffer = new ArrayBuffer(3)
+       const dataView = new DataView(buffer);
+       console.log(buffer)
+       dataView.setInt8(0, 256, unpacked)
+ 
+       const data0 = dataView.getInt8(0)
+       const data1 = dataView.getInt8(1)
+       console.log("Date time in Epoch: " + currentTime)
+       console.log(data0)
+       console.log(data1)
+       console.log(buffer)
+       */
 
       // const data = new Uint8Array(buffer, currentTime)
       // console.log(data)
-      await writeC.writeValue(jepa)
-      console.log("We wrote something to WRITE characteristic. We cannot read it tho...")
+
 
       // This function is called when the badge disconnects from the application
       function onDisconnected() {
@@ -103,8 +133,10 @@ function App() {
 
       // Here is a function to be called every 3 seconds to keep the connection up
       function communicateWithBadge() {
-        console.log(alertValue.getUint8(0))
+        console.log(date)
         console.log(currentTime)
+        sendDataRequest()
+        console.log("We wrote something to WRITE characteristic. We cannot read it tho...")
       }
 
     }
@@ -113,6 +145,11 @@ function App() {
     }
   }
 
+
+  //This function disconnects one badge
+  function disconnectBadge() {
+    console.log("disconnect")
+  }
   // This is a function which should convert the data types to values we could use in the front end.
   //This should be called when we add an event listener for future notifications.
   function handleCharacteristicValueChanged(event) {
@@ -121,7 +158,7 @@ function App() {
     console.log(badgeValue)
     console.log('Hello, I am event handler. Nice to meet ya :-)')
     // This sets badge data to the data we get from badges.
-    setBadgedata("badgeValue")
+    setBadgedata(badgeValue.getUint8(1))
   }
 
   return (
@@ -130,8 +167,13 @@ function App() {
         <h2>
           Hub
         </h2>
+        <h4>Time <br /> {time}</h4>
+        <div>
+
+        </div>
         <div className="buttonGroup">
           <button className="button" onClick={connectToDeviceAndSubscribeToUpdates}>Connect</button>
+          <button className="button2" onClick={disconnectBadge}>Disconnect</button>
           <p>Badge data: {badgedata}</p>
         </div>
       </header>
